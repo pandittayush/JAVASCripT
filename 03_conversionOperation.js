09118df9ef1c6959e892526da5694b7fe3719a9f
ayush pandey <ayushpandey@ayushs-MacBook-Air-2.local>
2024-02-05 13:48:23 +0530

    hmm

---
+++ 03_conversionOperation.js
@@ -0,0 +1,59 @@
+// let score = "33abc"
+// let score = null
+// let score = "ayush"
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// notes~~~~~~~~~~~
+
+// "33" => 33
+
+// "33abc" => NaN
+
+// true =>; false
+
+
+
+
+// let islogggedIn = "ayush"     =>true
+// let islogggedIn = ""    =>false
+// let islogggedIn = "1"    true
+
+// let booleanIsLoggedIn = Boolean(islogggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+
+// *********Operations*********
+
+let value = 3
+let negValue = -value
+// console.log(negValue); ,,=> -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hwello ji"
+let str2 = "Ayush"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(2 + 2 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+
+
---
+++ variable.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//  //we are using node.js not browser
+
+console.log(3+3)
+
+console.log("Heitesh")
+
+let name = "ayush"
+let age = 18
+let islogggedIn = false
+let state = null
+
+
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+
+
+console.log(typeof "hitesh")
