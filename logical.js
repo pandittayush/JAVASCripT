2511d46a2df835d716062894b115492cc9fbbe88
Ayush Pandey <119067526+pandittayush@users.noreply.github.com>
2024-02-11 19:44:31 +0530

    Update logical.js

--- logical.js
+++ logical.js
@@ -6,7 +6,6 @@
 //     console.log("yoou are no one")
 // }
 
-
 let age = prompt("What is your age")
 age = Number.parseInt(age)  //if i will not write this then you have to write the cases in string ('')like that
 switch(age){
