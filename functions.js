cf4634e4d8b8926c136d7bc330101770db9a38eb
Ayush Pandey <119067526+pandittayush@users.noreply.github.com>
2024-02-11 19:40:25 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -7,6 +7,6 @@ let b = 2;
 let c = 3;
 
 
-console.log("Average of a and b is", onePlusAvg(a,b))
+console.log("Average of a and b is", onePlusAvg(a,b));
 console.log("Average of b and c is", onePlusAvg(b,c))
 console.log("Average of c and a is", onePlusAvg(c,a))
